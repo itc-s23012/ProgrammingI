cb0ee7db6987e6cf03d2f6c052e72822efcc270d
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2023-07-06 07:56:51 +0000

    CHAPTER02/

---
+++ CHAPTER02/Q2_7_1.py
@@ -0,0 +1,12 @@
+a = ["orange", "apple", "grape", "banana"]
+print(a)
+
+a = ["orange", "apple", "grape"]
+a.append("banana")
+print(a)
+
+a = []
+a.append("orange")
+a.append("apple")
+a.append("grape")
+a.append("banana")
---
+++ CHAPTER02/Q2_7_2.py
@@ -0,0 +1,9 @@
+a = ["orange", "apple", "grape", "banana"]
+newa = sorted(a)
+print(newa)
+print(a)
+
+a = ["orange", "apple", "grape", "banana"]
+newa = sorted(a, reverse=True)
+print(newa)
+print(a)
