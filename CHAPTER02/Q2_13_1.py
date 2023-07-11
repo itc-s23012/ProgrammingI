6c483ca6b21abea823d0e1023adeead97e1e40b0
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2023-07-11 07:22:15 +0000

    CHAPTER02/

---
+++ CHAPTER02/Q2_13_1.py
@@ -0,0 +1,7 @@
+from math import pow
+
+print(pow(2, 3))
+
+print(pow(2, 3))
+
+print(pow(2.0, 3))
---
+++ CHAPTER02/Q2_13_2.py
@@ -0,0 +1,7 @@
+import random
+from random import randint
+
+print(random.randint(0, 10))
+print(randint(0, 10))
+
+# 出力結果が毎回異なる
