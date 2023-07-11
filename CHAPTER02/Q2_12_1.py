2312531381647d4957e8fce981523ea5d4d00858
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2023-07-11 07:11:55 +0000

    CHAPTER02/

---
+++ CHAPTER02/Q2_12_1.py
@@ -0,0 +1,7 @@
+import math
+
+print(math.pow(2, 3))  # 2の3乗される
+
+print(2**3)
+
+print(pow(2, 3))
---
+++ CHAPTER02/Q2_12_2.py
@@ -0,0 +1,7 @@
+# print(random.randint(0, 10))
+
+# 　エラー
+#
+import random
+
+print(random.randint(0, 10))
