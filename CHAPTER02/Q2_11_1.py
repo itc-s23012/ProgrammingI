17fbc7d509e2eeb1084d7a6141039669b92e183b
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2023-07-11 06:57:50 +0000

    CHAPTER02/

---
+++ CHAPTER02/Q2_11_1.py
@@ -0,0 +1,4 @@
+a, b, c, d = 1, 2, 3, 4
+a, b, c, d = c, d, a, b  # a=3, b=4, c=1, d=2
+print(c)
+# 1
---
+++ CHAPTER02/Q2_11_2.py
@@ -0,0 +1,7 @@
+a = 1
+b = 2
+a, b = b, a + b  # a=2, b=3
+a, b = b, a + b  # a=3, b=5
+print(a)
+print(b)
+# 3
